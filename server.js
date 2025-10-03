e6c93bdbed29b7d20aaade8c4e71c44bfd0168dd
Arps <arps@examplemail.ee>
2025-10-03 11:59:52 +0300

    changes to file structure

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const recipeRouter = require('./routes/recipes.routes');
 const ingredientRouter = require('./routes/ingredients.routes');
 const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const randomRouter = require('./routes/randomRecipe.routes');
 const todoRouter = require('./routes/todo.routes');
+const defaultRouter = require('./routes/default.routes')
 const app = express();
 
-app.use(express.json())
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -19,6 +24,7 @@ app.use('/recipes', recipeRouter);
 app.use('/fullRecipes', fullRecipesRouter);
 app.use('/random', randomRouter);
 app.use('/todo', todoRouter);
+app.use('/', defaultRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on Port 3000.');
